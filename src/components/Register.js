b9ad379960d0613a2ece67ea1f25c8d6f8d286b6
pikann <hvhai22@gmail.com>
2021-03-30 17:41:29 +0700

    Update link

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,14 +12,14 @@ class Register extends Component {
                         <img src="https://res.cloudinary.com/pikann22/image/upload/v1617098321/toptimviec/Pointofsale_jobs_f78ajf.jpg" className="mt-sm-3"></img>
                         <div className="card-body center-text">
                             <h5 className="card-title">Người tìm việc</h5>
-                            <a href="#" className="btn btn-success">Đăng ký Người tìm việc</a>
+                            <Link to="/dang-ky/nguoi-tim-viec" className="btn btn-success">Đăng ký Người tìm việc</Link>
                         </div>
                     </div>
                     <div className="col-sm-5 offset-sm-2 card mt-sm-5">
                         <img src="https://res.cloudinary.com/pikann22/image/upload/v1617098321/toptimviec/Best-Applicant-Tracking-Systems-Workable-vs-BreezyHR-vs-ApplicantPro_zam89c.png" className="mt-sm-3"></img>
                         <div className="card-body center-text">
                             <h5 className="card-title">Nhà tuyển dụng</h5>
-                            <a href="#" className="btn btn-success">Đăng ký Nhà tuyển dụng</a>
+                            <Link to="/dang-ky/nha-tuyen-dung" className="btn btn-success">Đăng ký Nhà tuyển dụng</Link>
                         </div>
                     </div>
                 </div>
