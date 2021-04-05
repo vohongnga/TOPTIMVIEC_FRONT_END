0685f90fe3b9f5de9f5b9d7ea09f82a18bbb35f7
pikann <hvhai22@gmail.com>
2021-04-05 14:49:54 +0700

    Merge from api-login

--- src/constants/ApiUrls.js
+++ src/constants/ApiUrls.js
@@ -1,12 +1,6 @@
 const ApiUrls = {
     "TEST_API": "https://toptimviec-backend.herokuapp.com/list-hashtag",
-<<<<<<< HEAD
-}
-
-export default ApiUrls
-=======
     "LOGIN_API": "https://toptimviec-backend.herokuapp.com/login",
 }
 
 export default ApiUrls;
->>>>>>> develop
